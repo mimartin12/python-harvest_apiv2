05b02b90d4247557d57abfdbb259b7e70c91a90d
Bradley van Ree <38775942+Brad-eki@users.noreply.github.com>
2020-10-30 18:15:36 +1100

    Roll version number
    Co-Authored-By: Bradley van Ree <47906258+bradbase@users.noreply.github.com>

--- harvest/metadata.py
+++ harvest/metadata.py
@@ -1,7 +1,7 @@
 
 # Copyright 2020 Bradbase
 
-__version__ = "1.7.0"
+__version__ = "1.8.0"
 __author__ = "Bradley van Ree"
 __copyright__ = "Copyright 2020 Bradbase"
 __maintainer__ = "Bradley van Ree"
